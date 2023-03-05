a3554709289b47151dadaefad3f780b889ad6f61
amit369 <amitchaman369@gmail.com>
2023-03-05 21:43:59 +0530

    changes done

---
+++ DataType.js
@@ -0,0 +1,14 @@
+var num1=10;
+var num2=25;
+console.log(num1, num2);
+
+var str1="This is a string";
+var str2="This is also a string";
+console.log(str1, str2);
+
+var marks = {
+    ravi : 34,
+    shubham : 78,
+    harry : 98
+}
+console.log(marks);
